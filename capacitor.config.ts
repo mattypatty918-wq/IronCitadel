63af78af0542b1f09cbe650aed71ec8a68e55046
Mathew Simpson <mattypattysapps@zo.computer>
2026-05-21 19:30:35 +0000

    Rename to Iron Citadel - updated app ID and name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mattypattysapps.nexusdefense',
-  appName: 'Nexus Defense',
+  appId: 'com.mattypattysapps.ironcitadel',
+  appName: 'Iron Citadel',
   webDir: 'dist'
 };
 
